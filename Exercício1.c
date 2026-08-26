cad6cfe51e9fef8ccc9546c8eeecbba5401a3442
Caio Brayner <caio07brayner@gmail.com>
2026-08-26 19:12:45 -0300

    adiciona 3 exercicios

---
+++ Exercício1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int ano_de_nascimento = 2006;
+
+    printf("O ano que você nasceu é : %d\n", ano_de_nascimento);
+
+    system("PAUSE");
+    return 0;
+}
